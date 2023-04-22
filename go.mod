b2c5f66d33625c54e089be321833643dae1dfedc
liutianq <liutianq@gmail.com>
2023-04-22 14:07:14 +0000

    1. v0.0.1

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/liutianq/hello
+
+go 1.20
+
+require (
+	github.com/liutianq/greetings v0.0.0-20230422135908-225d92d85efe
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ hello.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "rsc.io/quote"
+    "github.com/liutianq/greetings"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+
+    fmt.Println("\n")
+
+    fmt.Println(quote.Go())
+
+    fmt.Println("\n")
+
+    var message string
+    var err error
+    message, err = greetings.Hello("Agnese")
+    fmt.Println(message)
+
+    fmt.Println("\n")
+
+    log.SetPrefix("greetings: ")
+    log.SetFlags(17)
+    message, err = greetings.Hello("Elizabeth")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(message)
+
+    fmt.Println("\n")
+
+    names := []string{"A", "Gladys", "C", "Samantha", "Darrin"}
+
+    messages, err, seq := greetings.Hellos(names)
+    if err != nil {
+        fmt.Println(fmt.Sprintf("The first empty seq#: %v !\n", seq))
+        log.Fatal(err)
+    }
+    //fmt.Println(messages)
+    for _, message = range messages {
+        fmt.Println(message)
+    }
+}
