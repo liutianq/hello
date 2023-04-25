6a5ffaffbc2c2a59bf9dc15fa0b533f9d435d833
liutianq <liutianq@gmail.com>
2023-04-25 17:43:01 +0800

    1. gofmt 2. Upgrade package: greetings

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/liutianq/hello
 go 1.20
 
 require (
-	github.com/liutianq/greetings v0.0.0-20230422135908-225d92d85efe
+	github.com/liutianq/greetings v0.0.0-20230425084936-b6b9d26744de
 	rsc.io/quote v1.5.2
 )
 
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	rsc.io/sampler v1.99.99 // indirect
 )
--- hello.go
+++ hello.go
@@ -1,47 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "rsc.io/quote"
-    "github.com/liutianq/greetings"
+	"fmt"
+	"log"
+
+	"github.com/liutianq/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
 
-    fmt.Println("\n")
+	fmt.Println("\n")
 
-    fmt.Println(quote.Go())
+	fmt.Println(quote.Go())
 
-    fmt.Println("\n")
+	fmt.Println("\n")
 
-    var message string
-    var err error
-    message, err = greetings.Hello("Agnese")
-    fmt.Println(message)
+	var message string
+	var err error
+	message, err = greetings.Hello("Agnese")
+	fmt.Println(message)
 
-    fmt.Println("\n")
+	fmt.Println("\n")
 
-    log.SetPrefix("greetings: ")
-    log.SetFlags(17)
-    message, err = greetings.Hello("Elizabeth")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(17)
+	message, err = greetings.Hello("Elizabeth")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 
-    fmt.Println("\n")
+	fmt.Println("\n")
 
-    names := []string{"A", "Gladys", "C", "Samantha", "Darrin"}
+	names := []string{"A", "Gladys", "C", "Samantha", "Darrin"}
 
-    messages, err, seq := greetings.Hellos(names)
-    if err != nil {
-        fmt.Println(fmt.Sprintf("The first empty seq#: %v !\n", seq))
-        log.Fatal(err)
-    }
-    //fmt.Println(messages)
-    for _, message = range messages {
-        fmt.Println(message)
-    }
+	messages, err, seq := greetings.Hellos(names)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("The first empty seq#: %v !\n", seq))
+		log.Fatal(err)
+	}
+	//fmt.Println(messages)
+	for _, message = range messages {
+		fmt.Println(message)
+	}
 }
